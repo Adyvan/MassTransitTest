be1f0fb7db6de991c8b159a47b3a5a1ad711243f
GODELTECH\u.adushkevich <vladimir.adushkevich@gmail.com>
2022-06-06 14:12:57 +0200

    fix migration

--- MassTransitTest/Database/Migrations/OrderTablesMigration.cs
+++ MassTransitTest/Database/Migrations/OrderTablesMigration.cs
@@ -9,7 +9,7 @@ public class OrderTablesMigration : Migration
     public override void Up()
     {
         Create.Table(nameof(OrderSaga))
-            .WithColumn(nameof(OrderSaga.Id)).AsInt64().PrimaryKey().Identity()
+            .WithColumn(nameof(OrderSaga.Id)).AsInt64().Identity().PrimaryKey()
             .WithColumn(nameof(OrderSaga.OrderNumber)).AsInt64().NotNullable()
             .WithColumn(nameof(OrderSaga.OrderDate)).AsDateTime().NotNullable()
             .WithColumn(nameof(OrderSaga.UpdatedDate)).AsDateTime().NotNullable()
@@ -18,7 +18,7 @@ public class OrderTablesMigration : Migration
             .WithColumn(nameof(OrderSaga.ShippedDate)).AsDateTime().Nullable();
 
         Create.Table(nameof(OrderSagaItem))
-            .WithColumn(nameof(OrderSagaItem.Id)).AsInt64().NotNullable()
+            .WithColumn(nameof(OrderSagaItem.Id)).AsInt64().Identity().PrimaryKey()
             .WithColumn(nameof(OrderSagaItem.OrderId)).AsInt64().NotNullable()
             .WithColumn(nameof(OrderSagaItem.Sku)).AsInt32().NotNullable()
             .WithColumn(nameof(OrderSagaItem.Price)).AsDecimal().NotNullable()
